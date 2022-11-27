ca996e3071ca1c1296b286743424d59ab12cc12d
Carlos Gabriel de Oliveira Frazao <carlos.gofrazao@gmail.com>
2022-11-27 11:35:18 -0300

    commissioning to place pieces

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,7 +5,6 @@ import boardgame.Piece;
 
 public class ChessPiece extends Piece{
     private Color color;
-    private int moveCount;
 
     public ChessPiece(Board board, Color color){
         super(board);
